ab7cf912baa02f367ffc283603aa293b1881912f
mramos.dev423 <mramosg@fcpn.edu.bo>
2023-05-02 20:39:06 -0400

    asincronia

---
+++ JSON.js
@@ -0,0 +1,16 @@
+const usuarios=`{
+    "usuarios": [
+        {
+            "primerNombre":"Maria1",
+            "segundoNombre":"Paola1",
+        },
+        {
+            "primerNombre":"Maria2",
+            "segundoNombre":"Paola2",
+        },
+        {
+            "primerNombre":"Maria3",
+            "segundoNombre":"Paola3",
+        }
+    ]
+}`
