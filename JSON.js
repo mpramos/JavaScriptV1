810e918066d471bbed0bd03fbbdca0aa38d6df4f
mramos.dev423 <mramosg@fcpn.edu.bo>
2023-05-02 20:44:08 -0400

    json

--- JSON.js
+++ JSON.js
@@ -1,16 +1,24 @@
-const usuarios=`{
-    "usuarios": [
-        {
-            "primerNombre":"Maria1",
-            "segundoNombre":"Paola1",
-        },
-        {
-            "primerNombre":"Maria2",
-            "segundoNombre":"Paola2",
-        },
-        {
-            "primerNombre":"Maria3",
-            "segundoNombre":"Paola3",
-        }
+const usuariosText=`{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
     ]
-}`
+    }`
+const usuarioObjeto= JSON.parse(usuariosText)
+console.log(usuarioObjeto);
